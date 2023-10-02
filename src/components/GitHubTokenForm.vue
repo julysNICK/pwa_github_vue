390fc74288fea910c8ce73588c5a3448d6e6c156
julysNICK <julystube@gmail.com>
2023-10-02 11:52:55 -0300

    add repos component

---
+++ src/components/GitHubTokenForm.vue
@@ -0,0 +1,43 @@
+<template>
+  <form @submit.prevent="saveToken">
+    <div>
+      <label for="token">GitHub Token</label>
+      <br />
+      <input type="text" id="token" v-model="token" />
+    </div>
+
+    <div>
+      <input type="submit" value="Save Token" />
+
+      <button type="button" @click="resetToken">Reset Token</button>
+    </div>
+
+  </form>
+</template>
+
+<script>
+export default {
+  name: "GitHubTokenForm",
+  data() {
+    return {
+      token: ""
+    };
+  },
+
+  beforeMount() {
+    this.token = localStorage.getItem("github-token");
+  },
+
+  methods: {
+    saveToken() {
+      localStorage.setItem("github-token", this.token);
+    },
+
+    resetToken() {
+      localStorage.removeItem("github-token");
+      this.token = "";
+    }
+  }
+
+}
+</script>
